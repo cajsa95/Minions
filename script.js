3fe04709b41c5cb1da1ede42376af42be21cf5e7
Ylva Heise <ylva.heise2@gmail.com>
2018-03-05 12:40:26 +0100

    Revert "git conflict"
    This reverts commit 345d8662d3883d6270f0f335fb7668483a7c842d.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 $("#translate-form").on("submit", function(e) {
             e.preventDefault();
 
@@ -34,28 +35,31 @@ $("#translate-form").on("submit", function(e) {
             } else {
                 $("#text").removeClass("is-invalid");
             }
-        }); $("#translate-form").on("submit", function(e) {
-            e.preventDefault();
-
-            //hämtar texten som användaren skrivit in och sparar i en variabel
-            var text = $("#text").val();
-
-            //om användaren inte angivit någon text läggs följande klass för felmeddelande på
-            if (text == "") {
-                $("#text").addClass("is-invalid");
-                return false;
-            }
         });
+=======
+$("#translate-form").on("submit", function(e){
+  e.preventDefault();
 
-        //när användaren börjar skriva i textfältet så ska felmeddelandet försvinna
-        $("#text").on("keyup", function() {
-            if ($(this).val() == "") {
-                $("#text").addClass("is-invalid");
-            } else {
-                $("#text").removeClass("is-invalid");
-            }
-        });
+  //hämtar texten som användaren skrivit in och sparar i en variabel
+  var text = $("#text").val();
 
-        var translated = 'Banana I want a banana';
+  //om användaren inte angivit någon text läggs följande klass för felmeddelande på
+  if (text == ""){
+    $("#text").addClass("is-invalid");
+    return false;
+  }
+});
 
-        document.getElementById('play').addEventListener("click", function() { responsiveVoice.speak(translated, "Indonesian Female", { pitch: 1.7 }, { volume: 1 }); }, false);
+//när användaren börjar skriva i textfältet så ska felmeddelandet försvinna
+$("#text").on("keyup", function(){
+  if($(this).val() == ""){
+    $("#text").addClass("is-invalid");
+  } else{
+    $("#text").removeClass("is-invalid");
+  }
+});
+
+var translated = 'Banana I want a banana';
+
+document.getElementById('play').addEventListener("click", function(){responsiveVoice.speak(translated, "Indonesian Female", {pitch: 1.7}, {volume: 1});}, false);
+>>>>>>> master
