345d8662d3883d6270f0f335fb7668483a7c842d
Ylva Heise <ylva.heise2@gmail.com>
2018-03-05 12:04:19 +0100

    git conflict

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $("#translate-form").on("submit", function(e) {
             e.preventDefault();
 
@@ -35,31 +34,28 @@ $("#translate-form").on("submit", function(e) {
             } else {
                 $("#text").removeClass("is-invalid");
             }
+        }); $("#translate-form").on("submit", function(e) {
+            e.preventDefault();
+
+            //hämtar texten som användaren skrivit in och sparar i en variabel
+            var text = $("#text").val();
+
+            //om användaren inte angivit någon text läggs följande klass för felmeddelande på
+            if (text == "") {
+                $("#text").addClass("is-invalid");
+                return false;
+            }
         });
-=======
-$("#translate-form").on("submit", function(e){
-  e.preventDefault();
 
-  //hämtar texten som användaren skrivit in och sparar i en variabel
-  var text = $("#text").val();
+        //när användaren börjar skriva i textfältet så ska felmeddelandet försvinna
+        $("#text").on("keyup", function() {
+            if ($(this).val() == "") {
+                $("#text").addClass("is-invalid");
+            } else {
+                $("#text").removeClass("is-invalid");
+            }
+        });
 
-  //om användaren inte angivit någon text läggs följande klass för felmeddelande på
-  if (text == ""){
-    $("#text").addClass("is-invalid");
-    return false;
-  }
-});
+        var translated = 'Banana I want a banana';
 
-//när användaren börjar skriva i textfältet så ska felmeddelandet försvinna
-$("#text").on("keyup", function(){
-  if($(this).val() == ""){
-    $("#text").addClass("is-invalid");
-  } else{
-    $("#text").removeClass("is-invalid");
-  }
-});
-
-var translated = 'Banana I want a banana';
-
-document.getElementById('play').addEventListener("click", function(){responsiveVoice.speak(translated, "Indonesian Female", {pitch: 1.7}, {volume: 1});}, false);
->>>>>>> master
+        document.getElementById('play').addEventListener("click", function() { responsiveVoice.speak(translated, "Indonesian Female", { pitch: 1.7 }, { volume: 1 }); }, false);
