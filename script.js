4b0c910ad3d2bb9ba1e918d238d7ff37a39947ae
ah2333 <ellinor.maveus@hotmail.com>
2018-03-07 00:11:10 +0100

    Updates

--- script.js
+++ script.js
@@ -20,6 +20,23 @@ $("#text").on("keyup", function() {
     }
 });
 
+//sparar input i variabel
 var translated = 'Banana I want a banana';
 
+//väljer ut Id och vid klick körs speak-funktionen med valda parametrar
 document.getElementById('play').addEventListener("click", function() { responsiveVoice.speak(translated, "Indonesian Female", { pitch: 1.7 }, { volume: 1 }); }, false);
+
+$("#minion-quote").on("click", function(e){
+  console.log("hej");
+  
+  $.ajax({
+    url: "quotes.json",
+    contentType: false,
+    dataType: "JSON"
+  }).done(function(data){
+    console.log(data);
+  }).fail(function(data){
+    console.log("hejdå");
+  })
+
+});
